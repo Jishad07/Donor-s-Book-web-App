2b7ded2057cf6adde8625715eb693b3b5e8034f6
Jishad07 <jishadajd@gmail.com>
2024-05-14 01:01:29 +0530

    firebase added

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/donaters_page.dart';
 
@@ -9,6 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final CollectionReference donor=FirebaseFirestore.instance.collection('Donor');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +24,78 @@ class _HomeState extends State<Home> {
         children: [
           Expanded(
             flex: 10,
-             child:Container(
-              color: Colors.amber,
-             )),
+             child:StreamBuilder(
+              stream: donor.snapshots(),
+              builder:(context,AsyncSnapshot snapshot) {
+                if(snapshot.hasData){
+                  return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot donorsdata=snapshot.data.docs[index];
+                    return  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        // height: 80,
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    radius: 30,
+                                    child: Center(child: Text(donorsdata['bloodgroup'])),
+                                  ),
+                                ),
+                                SizedBox(height: 10,),
+                                 Container(
+                                 
+                                  // color: Colors.amber,
+                                 child: const Column(
+                                  children: [
+                                    Text("Donor's Details"),
+                                    Divider(
+                                      
+                                      color: Colors.black,
+                                    )
+                                  ],
+                                 ),
+                                )
+                           
+                              
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(donorsdata['name'],style:const TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                                Text(donorsdata['phone'].toString())
+                              ],
+                            ),
+                             Row(
+                              children: [
+                                IconButton(onPressed: (){}, icon: const Icon(Icons.edit_note_outlined)),
+                                  IconButton(onPressed: (){}, icon:const  Icon(Icons.delete,),color: Colors.red,)
+                              ],
+                              
+                             ), 
+                             
+                          ],
+                          
+                        ),
+                        
+                      ),
+                      
+                    );
+                  },);
+                }
+                return Container(
+                  child: Text("mujeebaka"),
+                );
+              },)
+             ),
              Expanded(
               flex: 1,
               child: Container(
